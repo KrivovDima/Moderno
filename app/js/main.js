dc1c6f9bc1d8a3148170fab8fd49fbac833532d2
KrivovDima <krivovdima89@gmail.com>
2021-01-23 14:06:09 +0400

    added products-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,13 @@
 $(function () {
 
-
     var mixer = mixitup('.products__inner');
 
-
-
-
-
+    $('.products-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false
+      });
 
 
 });
